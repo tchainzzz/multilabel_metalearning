802f01c3fc6beef501ef9b2ba8c5ecbbc50c541e
Trenton Chang <tchang97@stanford.edu>
2020-11-17 01:26:46 -0800

    ablation scripts

--- maml_ablations.sh
+++ maml_ablations.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 set -x
-python -u maml.py --data-root ../cs330-storage --support-size 8 --label-subset-size 3 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 16 --label-subset-size 3 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 32 --label-subset-size 3 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 4 --label-subset-size 3 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 8 --label-subset-size 4 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 8 --label-subset-size 5 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+python -u maml.py --data-root ../cs330-storage --support-size 8 --label-subset-size 6 --multilabel-scheme binary --sampling-mode permutation --log-frequency 10 --iterations 10000 --learn-inner-lr
+
+
